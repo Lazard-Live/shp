c22abf827a7f0ef257ad5f82413fb5c7a75ff702
DoretskiyBV <63367508+Lazard-Live@users.noreply.github.com>
2023-07-18 13:04:05 +0300

    Внесение краткой тестовой информации о ресурсах

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,30 @@
 import './App.css'
-import {Link} from "react-router-dom";
 
 function App() {
 
     return (
         <>
             <div>
-                <h3>Добро пожаловать на тестовую распределительную стараницу</h3>
+                <p>Добро пожаловать на тестовую распределительную стараницу</p>
 
-                <div>1. <a href={'https://78.36.1.133:80/'}>Proxmax VM</a></div>
-                <div> 2. <a href={'http://78.36.1.133:8080/'}> Torrent WGUi</a></div>
+                <div>Полезные ссылки
+                    <div>1. <a href={'https://78.36.1.133:80/'}>Proxmax VM</a></div>
+                    <div>2. <a href={'http://78.36.1.133:8080/'}> Torrent WEB</a></div>
+                </div>
+                <div>Игровые серверы:
+                    <div>Minecraft: 78.36.1.133:3003
+                        <div>- Версия игры: Forge 1.20.1</div>
+                        <div>- Вход по белому листу</div>
+                    </div>
+                </div>
 
+                <div>
+                    <a href={'https://discord.gg/D7rskdhQ'}>Сообщество Discord</a>
+
+                </div>
 
             </div>
+
         </>
     )
 }
