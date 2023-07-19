ebaf2a6342131af5fea98d0b9a7cdd21f3228f47
DoretskiyBV <63367508+Lazard-Live@users.noreply.github.com>
2023-07-19 12:46:35 +0300

    Добавил тестовый стиль card и дополнил перемнные

--- src/App.jsx
+++ src/App.jsx
@@ -2,24 +2,51 @@ function App() {
 
     return (
         <>
-            <div>
-                <p>Добро пожаловать на тестовую распределительную стараницу</p>
-
-                <div>Полезные ссылки
-                    <div>1. <a href={'https://78.36.1.133:80/'}>Proxmax VM</a></div>
-                    <div>2. <a href={'http://78.36.1.133:8080/'}> Torrent WEB</a></div>
-                </div>
-                <div>Игровые серверы:
-                    <div>Minecraft: 78.36.1.133:3003
-                        <div>- Версия игры: Forge 1.20.1</div>
-                        <div>- Вход по белому листу</div>
+            <div className={'container'}>
+                <div className="row">
+                    <div className="col-sm-12">
+                        <h1>Добро пожаловать на тестовую распределительную стараницу</h1>
                     </div>
                 </div>
+            </div>
+            <div className={'container'}>
+                <div className={'row'}>
+                    <div className={'col-sm-12 col-md-6'}>
+                        <div className={'card'}>
+                            <div className="card__title">Полезные ссылки</div>
+                            <div className="card__text">
+                                <div className={'card__link'}>
+                                    1.<a href={'https://78.36.1.133:443/'}>
+                                    Proxmax VM</a>
+                                </div>
+                                <div className={'card__link'}>
+                                    2.<a href={'http://78.36.1.133:8080/'}>
+                                    Torrent WEB
+                                </a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
 
-                <div>
-                    <a href={'https://discord.gg/D7rskdhQ'}>Сообщество Discord</a>
+                    <div className={'col-md-6 col-lg-6'}>
+                        <div className={'card'}>
+                            <div className="card__title">
+                                Игровые серверы
+                            </div>
+                            <div className="card__text">
+                                <div>Minecraft: 78.36.1.133:3003
+                                    <div>Версия игры: Forge 1.20.1</div>
+                                    <div> Вход по белому листу</div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-
+                <div className={'row'}>
+                    <div className={'col'}>
+                        <a href={'https://discord.gg/D7rskdhQ'}>Сообщество Discord</a>
+                    </div>
+                </div>
             </div>
 
         </>
