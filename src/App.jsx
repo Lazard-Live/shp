82f9e69fe72aff99d8bb4ce46bc1030a6b8fd509
DoretskiyBV <63367508+Lazard-Live@users.noreply.github.com>
2023-07-21 13:12:28 +0300

    Рефактор структуры, фиксы App и card и убрал фон

--- src/App.jsx
+++ src/App.jsx
@@ -2,53 +2,64 @@ function App() {
 
     return (
         <>
-            <div className={'container'}>
+            <header className={'header'}>
                 <div className="row">
                     <div className="col-sm-12">
-                        <h1>Добро пожаловать на тестовую распределительную стараницу</h1>
+                        <h1 className={'title title_center'}>Выбери что нужно</h1>
                     </div>
                 </div>
-            </div>
-            <div className={'container'}>
-                <div className={'row'}>
-                    <div className={'col-sm-12 col-md-6'}>
-                        <div className={'card'}>
-                            <div className="card__title">Полезные ссылки</div>
-                            <div className="card__text">
-                                <div className={'card__link'}>
-                                    1.<a href={'https://78.36.1.133:443/'}>
-                                    Proxmax VM</a>
+
+            </header>
+
+            <section className={'section root__content'}>
+                <div className={'container'}>
+                    <div className={'row'}>
+                        <div className={'col-sm-12 col-md-6'}>
+                            <div className={'card'}>
+                                <div className={'card__img'}>
+                                    <div className="card__title">Домашние ресурсы</div>
                                 </div>
-                                <div className={'card__link'}>
-                                    2.<a href={'http://78.36.1.133:8080/'}>
-                                    Torrent WEB
-                                </a>
+
+                                <div className="card__text">
+                                    <div className={'card__link'}>
+                                        1.<a href={'https://78.36.1.133:443/'}>
+                                        Proxmax VM</a>
+                                    </div>
+                                    <div className={'card__link'}>
+                                        2.<a href={'http://78.36.1.133:8080/'}>
+                                        Torrent WEB
+                                    </a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className={'col-md-6 col-lg-6'}>
+                            <div className={'card'}>
+                                <div className={'card__img'}>
+                                    <div className="card__title">
+                                        Личные игровые серверы для relax'а
+                                    </div>
+                                </div>
+
+                                <div className="card__text">
+                                    <div>Minecraft: 78.36.1.133:3003
+                                        <div>Версия игры: Forge 1.20.1</div>
+                                        <div> Вход по белому листу</div>
+                                    </div>
                                 </div>
                             </div>
                         </div>
                     </div>
-
-                    <div className={'col-md-6 col-lg-6'}>
-                        <div className={'card'}>
-                            <div className="card__title">
-                                Игровые серверы
-                            </div>
-                            <div className="card__text">
-                                <div>Minecraft: 78.36.1.133:3003
-                                    <div>Версия игры: Forge 1.20.1</div>
-                                    <div> Вход по белому листу</div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
                 </div>
-                <div className={'row'}>
-                    <div className={'col'}>
-                        <a href={'https://discord.gg/D7rskdhQ'}>Сообщество Discord</a>
-                    </div>
-                </div>
-            </div>
+            </section>
 
+            <footer className={'footer'}>
+                <div className="container-fluid">
+                    <a className={'footer__link'} href={'https://discord.gg/D7rskdhQ'}>Discord</a>
+                    <a className={'footer__link'} href={'https://t.me/workbd'}>Telegram</a>
+                </div>
+            </footer>
         </>
     )
 }
